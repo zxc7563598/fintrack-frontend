23ce5fc71db99e5e0a0da4f7aa9ab298e0d35ffb
苏青安 <992182040@qq.com>
2025-09-18 15:10:08 +0800

    feat(core): 增加应用设置-邮件配置，实现配置邮件监听账单的基本功能

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -31,17 +31,43 @@
                     <div class="ml-3">{{ config.name }}</div>
                 </div>
             </v-app-bar-title>
+            <template v-slot:append>
+                <v-menu width="200">
+                    <template v-slot:activator="{ props }">
+                        <v-btn icon="mdi-account-outline" color="primary" v-bind="props" class="mr-3"></v-btn>
+                    </template>
+                    <v-list>
+                        <v-list-item :value="'user'" slim prepend-icon="mdi-account-cog-outline" color="primary"
+                            @click="userSettingsDialog = true">
+                            <v-list-item-title>用户设置</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item :value="'application'" slim prepend-icon="mdi-cog-outline" color="primary"
+                            @click="applicationSettingsDialog = true">
+                            <v-list-item-title>应用设置</v-list-item-title>
+                        </v-list-item>
+                        <v-divider></v-divider>
+                        <v-list-item :value="'loginout'" slim prepend-icon="mdi-account-arrow-down-outline"
+                            color="primary">
+                            <v-list-item-title>退出登录</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+            </template>
         </v-app-bar>
         <v-main class="app-main">
             <router-view />
         </v-main>
     </v-app>
+    <UserSettings v-model="userSettingsDialog"></UserSettings>
+    <ApplicationSettings v-model="applicationSettingsDialog" :option="'email'"></ApplicationSettings>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import config from '../static/config';
+import UserSettings from '@/components/settings/UserSettings.vue'
+import ApplicationSettings from '@/components/settings/ApplicationSettings.vue'
 
 const route = useRoute()
 
@@ -60,4 +86,8 @@ const menuItems = [
     { title: '交易详情', icon: 'mdi-list-status', path: '/details' },
     { title: '统计分析', icon: 'mdi-circle-slice-5', path: '/statistics' },
 ]
+
+// 控制弹窗
+const userSettingsDialog = ref(false)
+const applicationSettingsDialog = ref(false)
 </script>
